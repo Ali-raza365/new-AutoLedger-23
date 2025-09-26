34fb6c7f05489d302b8f405c7ff20dd424add4b1
Ali Haider <212581322+Alihaider-kS@users.noreply.github.com>
2025-09-26 15:59:38 +0500

    Remove build and distribution files
    Deleted all files from the dist and client/dist directories, including compiled JavaScript, CSS, HTML, and server assets. This cleanup likely prepares for a fresh build or addresses outdated build artifacts. Also updated package.json and vercel.json.

--- dist/server/services/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SeedService = void 0;
-// Export all services from a single entry point
-var seedService_1 = require("./seedService");
-Object.defineProperty(exports, "SeedService", { enumerable: true, get: function () { return seedService_1.SeedService; } });
